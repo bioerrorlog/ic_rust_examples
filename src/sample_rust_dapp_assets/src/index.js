d6c70f6264c97ed77760357eff0f06bd6fc7f6b4
bioerrorlog <n.iijima.0x01@gmail.com>
2022-03-10 09:34:56 +0900

    Initial commit.

---
+++ src/sample_rust_dapp_assets/src/index.js
@@ -0,0 +1,19 @@
+import { sample_rust_dapp } from "../../declarations/sample_rust_dapp";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await sample_rust_dapp.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});

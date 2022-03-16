9c521820137f82968e618ae4562c499cf3c115d1
bioerrorlog <n.iijima.0x01@gmail.com>
2022-03-16 09:40:45 +0900

    add jp test

--- hello/src/hello/lib.rs
+++ hello/src/hello/lib.rs
@@ -11,4 +11,11 @@ mod tests {
         let result = greet("World".to_string());
         assert!(result.contains("World"));
     }
+
+
+    #[test]
+    fn greeting_contains_jp_string() {
+        let result = greet("日本語の名前".to_string());
+        assert!(result.contains("日本語の名前"));
+    }
 }

d112be68465081e978effe3bd74c06c46d18dd59
bioerrorlog <bioerrorlog.contact@gmail.com>
2022-03-20 09:46:37 +0900

    add init

--- counter_simple/src/counter_simple/lib.rs
+++ counter_simple/src/counter_simple/lib.rs
@@ -1,5 +1,11 @@
 static mut CONTER: u64 = 0;
 
+#[ic_cdk_macros::init]
+fn init() {
+    unsafe {
+        CONTER = 0;
+    }
+}
 #[ic_cdk_macros::query]
 fn get() -> u64 {
     unsafe { CONTER }
@@ -17,15 +23,25 @@ mod tests {
     use super::*;
 
     #[test]
+    fn check_initial_get() {
+        init();
+        assert_eq!(get(), 0);
+    }
+
+    #[test]
     fn check_first_increment_validity() {
+        init();
         increment();
-        assert_eq!(unsafe {CONTER}, 1);
+        assert_eq!(unsafe { CONTER }, 1);
     }
 
     #[test]
     fn check_increment_validity_with_specific_status() {
-        unsafe { CONTER = 100; }
+        init();
+        unsafe {
+            CONTER = 100;
+        }
         increment();
-        assert_eq!(unsafe {CONTER}, 101);
+        assert_eq!(unsafe { CONTER }, 101);
     }
 }

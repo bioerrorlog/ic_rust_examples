c5ee0288a6cf634d6112fead146ac0522036f7bd
bioerrorlog <bioerrorlog.contact@gmail.com>
2022-03-20 10:57:53 +0900

    add actor tests

--- counter/src/counter/lib.rs
+++ counter/src/counter/lib.rs
@@ -38,4 +38,26 @@ mod tests {
         init();
         assert_eq!(get(), candid::Nat::from(0));
     }
+
+    #[test]
+    fn check_set_get_validity() {
+        init();
+        set(candid::Nat::from(100));
+        assert_eq!(get(), candid::Nat::from(100));
+    }
+
+    #[test]
+    fn check_initial_increment() {
+        init();
+        increment();
+        assert_eq!(get(), candid::Nat::from(1));
+    }
+
+    #[test]
+    fn check_setted_increment() {
+        init();
+        set(candid::Nat::from(1000));
+        increment();
+        assert_eq!(get(), candid::Nat::from(1001));
+    }
 }

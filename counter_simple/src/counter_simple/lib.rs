45e317116c87a2bb40a8af38aebc86834674bcbe
bioerrorlog <bioerrorlog.contact@gmail.com>
2022-03-20 10:07:07 +0900

    fix typo

--- counter_simple/src/counter_simple/lib.rs
+++ counter_simple/src/counter_simple/lib.rs
@@ -1,20 +1,20 @@
-static mut CONTER: u64 = 0;
+static mut COUNTER: u64 = 0;
 
 #[ic_cdk_macros::init]
 fn init() {
     unsafe {
-        CONTER = 0;
+        COUNTER = 0;
     }
 }
 #[ic_cdk_macros::query]
 fn get() -> u64 {
-    unsafe { CONTER }
+    unsafe { COUNTER }
 }
 
 #[ic_cdk_macros::update]
 fn increment() {
     unsafe {
-        CONTER += 1;
+        COUNTER += 1;
     }
 }
 
@@ -32,16 +32,16 @@ mod tests {
     fn check_first_increment_validity() {
         init();
         increment();
-        assert_eq!(unsafe { CONTER }, 1);
+        assert_eq!(unsafe { COUNTER }, 1);
     }
 
     #[test]
     fn check_increment_validity_with_specific_status() {
         init();
         unsafe {
-            CONTER = 100;
+            COUNTER = 100;
         }
         increment();
-        assert_eq!(unsafe { CONTER }, 101);
+        assert_eq!(unsafe { COUNTER }, 101);
     }
 }

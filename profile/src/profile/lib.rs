cfaa060ed6914937b81c63e75dda711c40ba080a
bioerrorlog <bioerrorlog.contact@gmail.com>
2022-03-27 15:31:30 +0900

    add types

--- profile/src/profile/lib.rs
+++ profile/src/profile/lib.rs
@@ -1,6 +1,21 @@
+use ic_cdk::{
+    export::{CandidType, Deserialize},
+    Principal,
+};
+use std::cell::RefCell;
+use std::collections::BTreeMap;
+
+type IdStore = BTreeMap<String, Principal>;
+type ProfileStore = BTreeMap<Principal, Profile>;
+
 #[derive(Clone, Debug, Default, CandidType, Deserialize)]
 struct Profile {
     pub name: String,
     pub description: String,
     pub keywords: Vec<String>,
+}
+
+thread_local! {
+    static PROFILE_STORE: RefCell<ProfileStore> = RefCell::default();
+    static ID_STORE: RefCell<IdStore> = RefCell::default();
 }
